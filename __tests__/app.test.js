66ad7bea387163a3157decf3ab0c58533b76159b
Alice Hsing <alicehsing@gmail.com>
2022-03-15 13:24:54 -0700

    refactored UpdateById() and deleteById() functions and tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -14,16 +14,16 @@ const Order = require('../lib/models/Order');
 // }
 
 // TODO: Remove this function & use the Order model
-async function getOrderById(id) {
-  const { rows } = await pool.query(
-    'SELECT * FROM orders WHERE id=$1;',
-    [id]
-  );
+// async function getOrderById(id) {
+//   const { rows } = await pool.query(
+//     'SELECT * FROM orders WHERE id=$1;',
+//     [id]
+//   );
 
-  if (!rows[0]) return null;
+//   if (!rows[0]) return null;
 
-  return new Order(rows[0]);
-}
+//   return new Order(rows[0]);
+// }
 
 describe('refactory routes', () => {
   beforeEach(() => {
@@ -54,7 +54,7 @@ describe('refactory routes', () => {
   });
 
   it('should be able to list orders', async () => {
-    await createOrder({ product: 'Widget', quantity: 1 });
+    await Order.insert({ product: 'Widget', quantity: 1 });
     const res = await request(app).get('/api/v1/orders');
 
     expect(res.body).toEqual([
@@ -67,7 +67,7 @@ describe('refactory routes', () => {
   });
 
   it('should be able to update an order', async () => {
-    const order = await createOrder({ product: 'Widget', quantity: 1 });
+    const order = await Order.insert({ product: 'Widget', quantity: 1 });
     const res = await request(app)
       .patch(`/api/v1/orders/${order.id}`)
       .send({ product: 'Thingamajig', quantity: 2 });
@@ -79,14 +79,14 @@ describe('refactory routes', () => {
     };
 
     expect(res.body).toEqual(expected);
-    expect(await getOrderById(order.id)).toEqual(expected);
+    expect(await Order.getById(order.id)).toEqual(expected);
   });
 
   it('should be able to delete an order', async () => {
-    const order = await createOrder({ product: 'Widget', quantity: 1 });
+    const order = await Order.insert({ product: 'Widget', quantity: 1 });
     const res = await request(app).delete(`/api/v1/orders/${order.id}`);
 
     expect(res.body).toEqual(order);
-    expect(await getOrderById(order.id)).toBeNull();
+    expect(await Order.getById(order.id)).toBeNull();
   });
 });
